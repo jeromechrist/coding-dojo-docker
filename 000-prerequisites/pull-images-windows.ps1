612e42a03341759480f821cef046b78283780e42
Jerome CHRIST <jerome.christ.c@thalesdigital.io>
2018-11-12 01:37:59 +0100

    added scripts to pre pull docker images

---
+++ 000-prerequisites/pull-images-windows.ps1
@@ -0,0 +1,1 @@
+docker pull microsoft/aspnet:4.7.1@sha256:d3534cac53bc3e13a707e41d8de931b91cc86d98021e2b4c643637ef666fe510
---
+++ 000-prerequisites/pull-images.ps1
@@ -0,0 +1,12 @@
+# linux based images
+docker pull microsoft/dotnet:2.1-sdk
+docker pull microsoft/dotnet:2.1-aspnetcore-runtime
+docker pull traefik
+docker pull jeromechrist/whoami:latest
+docker pull microsoft/vsts-agent:ubuntu-16.04
+docker pull sitespeedio/sitespeed.io
+docker pull owasp/zap2docker-weekly
+docker pull selenium/hub:3.13.0
+docker pull selenium/node-chrome-debug:3.13.0
+docker pull selenium/node-firefox-debug:3.13.0
+docker pull python:2.7-alpine
