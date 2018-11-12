a95e81295cf31bc731dce79d875f7329ea580006
Jérôme CHRIST <jerome.christ.c@thalesdigital.io>
2018-11-12 15:33:52 +0100

    use case sonarqube

---
+++ 106-use-case-sonarqube/aspnetapp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config)=>{ 
+                config.AddDockerSecrets(((c)=>{
+                    c.Optional = true;
+                }));
+            })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConsole();
+                logging.AddDebug();
+            })
+            .UseStartup<Startup>();
+    }
+}
